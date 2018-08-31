3dc3325a6bf0195d177b90551a6dfd3f14b426d1
zhangliangxiang <zhanglx@zhanglxdeMacBook-Pro.local>
2018-08-31 12:05:17 +0800

    code gen ok

--- update-codegen.sh
+++ update-codegen.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 vendor/k8s.io/code-generator/generate-groups.sh all \
-github.com/zhaglianx111/nfscontroller/pkg/client \
+github.com/zhanglianx111/nfscontroller/pkg/client \
 github.com/zhanglianx111/nfscontroller/pkg/apis \
 nfscontroller:v1alpha1
